6b3e86fff02cc4dd0ae360f0caad1d6ba08cc5f3
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-12-06 04:04:37 +0300

    created migration for appointment and migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
+  resources :appointments
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   #get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   resources :users, only: [:index]
-  
+
   post "/auth/signup", to: "users#create" 
-  post "/auth/login", to: "sessions#create"
+  post "/auth/signin", to: "sessions#create"
   delete "/auth/logout", to: "sessions#destroy"
 end
