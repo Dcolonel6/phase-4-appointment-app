ad19319c11128ee4fc785dad8ee9658fd1791f71
SirJimKe <jimmymutuku1@gmail.com>
2022-12-06 16:53:09 +0300

    Navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import Appointments from './pages/appointment';
+import Doctors from './pages/doctor';
+import SignUp from './pages/signup';
 import Auth from './components/authForm/Auth';
+
 
 function App() {
   return (
     <div className="App">
       <Auth />
+      <Router>
+      
+      <Navbar />
+      <Auth />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/appointments' element={<Appointments/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ client/src/components/authForm/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          BookADoc
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/appointments' activeStyle>
+            Appointment
+          </NavLink>
+          <NavLink to='/doctors' activeStyle>
+            Doctors
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/login'>Log In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
