3859b15b181c7e5e484c00ab3a45074a8849f9b1
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-12-06 13:48:13 +0300

    feature branch

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index]
+  resources :appointments
 
   post "/auth/signup", to: "users#create" 
   post "/auth/login", to: "sessions#create"
